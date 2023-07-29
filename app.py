cd1e9eef52b24c04b54551de3b4fb1193555eacc
Funklasthrones <43086861+Funklasthrones@users.noreply.github.com>
2023-07-29 13:21:42 +0000

    Another one

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/hello/<name>')
-def hello_name(name):
-return 'Hello %s!' % name
+@app.route('/')
+def hello_name():
+    return 'Hello'
 
 if __name__ == '__main__':
-app.run()
+    app.run()
